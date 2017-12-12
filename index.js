588309666b0a39748ed2fe3b430f8b796908e31c
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-12 20:09:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Route{
     this.beginningLocation = beginningLocation
     this.endingLocation = endingLocation
   }
-  let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+  
   //
   // blocksTravelled(){
   //   const beginningHorizontal = eastWest.indexOf(this.beginningLocation.horizontal)
