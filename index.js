3494615dc21d261bc125b19fd365897eeaa59125
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-12 19:55:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Driver{
     this.startDate = new Date(startDate)
   }
 
-  yearsExperienceFromBeginningOf(date){
+  yearExperienceFromBeginningOf(date){
     let endDate = new Date(year, 1, 1)
     let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
     return parseInt(totalYears)
